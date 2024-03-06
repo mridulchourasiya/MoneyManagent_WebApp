f667f4491f6788dbcc2960fac3165d11bbf6872c
Mridul chourasia <99160348+mridulchourasiya@users.noreply.github.com>
2024-03-06 18:29:41 +0530

    GraphQL Setup
    Import in envrioment file to exection and resolvation

---
+++ backend/typeDefs/index.js
@@ -0,0 +1,9 @@
+import { mergeTypeDefs } from "@graphql-tools/merge";
+
+// tyepe defs
+import userTypeDef from "./user.typeDef.js";
+import transactionTypeDef from "./transaction.typeDef.js";
+
+const mergedTypeDefs = mergeTypeDefs([userTypeDef, transactionTypeDef]);
+
+export default mergedTypeDefs;
